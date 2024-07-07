30f8bbd821aafbe6514c647142cab342e7e1c2a0
bdemissie <bemnet.hdemissie@gmail.com>
2024-07-07 18:48:24 -0400

    Comments remain

--- db/conn.mjs
+++ db/conn.mjs
@@ -5,7 +5,8 @@ dotenv.config();
 const user  = process.env.USER;
 const password = process.env.PASSWORD;
 
-let uri = `mongodb+srv://${user}:${password}@mongocluster.kpjim1y.mongodb.net/?retryWrites=true&w=majority&appName=MongoCluster`
+// let uri = `mongodb+srv://${user}:${password}@mongocluster.kpjim1y.mongodb.net/?retryWrites=true&w=majority&appName=MongoCluster`
+let uri = 'mongodb+srv://perScholasUser:EM3RhJuQ3GrYRzm9@mongocluster.kpjim1y.mongodb.net/?retryWrites=true&w=majority&appName=MongoCluster'
 const client = new MongoClient(uri);
 
 let conn;
--- routes/admin.mjs
+++ routes/admin.mjs
@@ -1,5 +1,6 @@
 import express from 'express';
 import db from "../db/conn.mjs";
+import { ObjectId } from 'mongodb';
 
 // Create a router module
 const router = express.Router();
@@ -42,23 +43,28 @@ router.post('/sign-in', async (req, res) => {
                 description: 'Get a list of user names, emails and usernames'
             },
             {   
+                method: "GET",
+                endpoint: '/admin/user/id/:id',
+                description: 'Get user by id'
+            },
+            {   
                 method: "DELETE",
-                endpoint: '/admin/user/:id',
+                endpoint: '/admin/user/id/:id',
                 description: 'Get user by id'
             },
             {
                 method: "GET",
-                endpoint: 'admin/:username',
+                endpoint: 'admin/user/username/:username',
                 description: "Get user by username",
             },
             {
                 method: "PATCH",
-                endpoint: 'admin/:username',
+                endpoint: 'admin/user/username/:username',
                 description: "Update user's username"
             },
             {
                 method: "PATCH",
-                endpoint: 'admin/:id/isadmin',
+                endpoint: 'admin/user/:id/:isadmin',
                 description: "Update user's admin privileges"
             },
             {
@@ -81,6 +87,154 @@ catch (err) {
     res.status(500).json({ message: 'Error signing in', error: err.message });
 }
 
+})
+
+router.get('/users', async (req, res) => {
+
+    try{
+
+        let collection = await db.collection('users');
+
+        let result = await collection
+        .aggregate(
+            [
+                {
+                  '$project': {
+                    '_id': 0, 
+                    'first_name': 1, 
+                    'last_name': 1, 
+                    'email': 1, 
+                    'username': 1
+                  }
+                }, {
+                  '$limit': 5
+                }
+              ]
+        ).toArray();
+        res.send(result);
+    }
+    catch  (err) {
+
+        res.status(500).json({ message: 'Something went wrong', error: err.message });
+
+    }
+})
+
+router.get('/user/id/:id', async (req, res) => {
+
+    try {
+
+        const collection = await db.collection('users');
+
+        const query = {_id: new ObjectId(req.params.id) };
+    
+        const result =  await collection.findOne(query);
+    
+        res.send(result).status(200)
+    }
+    catch (err) {
+
+            res.status(500).json({ message: 'Something went wrong', error: err.message });
+
+    }
+} )
+
+router.delete('/user/id/:id', async (req, res) => {
+
+    try {
+
+        const collection = await db.collection('users');
+
+        const query = {_id: new ObjectId(req.params.id) };
+    
+        const result =  await collection.deleteOne(query);
+    
+        res.status(200).json({result: 'user removed'})
+    }
+    catch (err) {
+
+            res.status(500).json({ message: 'Something went wrong', error: err.message });
+
+    }
+} )
+
+router.get('/user/username/:username', async (req, res) => {
+
+    try {
+
+        const collection = await db.collection('users');
+
+        const query = {username: req.params.username};
+    
+        const result =  await collection.findOne(query);
+    
+        res.send(result).status(200);
+    }
+    catch (err) {
+
+            res.status(500).json({ message: 'Something went wrong', error: err.message });
+
+    }
+} )
+
+
+router.delete('/user/username/:username', async (req, res) => {
+
+    try {
+
+        const collection = await db.collection('users');
+
+        const query = {username: req.params.username};
+    
+        const result =  await collection.deleteOne(query);
+    
+        res.status(200).json({result: 'user removed'})
+    }
+    catch (err) {
+
+            res.status(500).json({ message: 'Something went wrong', error: err.message });
+
+    }
+} )
+
+router.patch('/user/username/:username', async (req, res) => {
+
+    try {
+
+        const collection = await db.collection('users');
+
+        const query = {username: req.params.username};
+    
+        let result =  await collection.updateOne(query, {
+            $set: {username: req.params.newUsername}
+        }
+        );
+    
+        res.status(200).json({result: 'user name updated'})
+    }
+    catch (err) {
+
+            res.status(500).json({ message: 'Something went wrong', error: err.message });
+
+    }
+} )
+
+router.patch('/user/id/:id/:isadmin', async (req,res) => {
+
+    try {
+
+        const collection = await db.collection('users');
+
+        const query = {_id: new ObjectId(req.params.id)};
+
+        let result = await collection.updateOne(query, {
+            $set: {"admin": req.params.isadmin}
+        });
+        res.status(200).json({result: 'user admin privilage updated'})
+    }
+    catch (err) {
+        res.status(500).json({message: 'Something went wrong', error: err.message})
+    }
 })
 
 
